17b0ac817d3ce7fd347a93878c29bf6744f141db
Nick <nickf1574@gmail.com>
2025-03-06 13:33:35 -0800

    update

---
+++ week9/battleship.js
@@ -0,0 +1,32 @@
+let location1 = Math.floor(Math.random() * 5);
+// Math.floor(min + Math.random() * max-min+1)
+let location2 = location1 + 1;
+let location3 = location2 + 1;
+let guess;
+let hits = 0;
+let guesses = 0;
+let isSunk = false;
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!")
+    }
+    else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
+            hits = hits + 1
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!")
+            }
+        }
+        else {
+            alert("MISS!")
+        }
+    }
+}
+
+let stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + Math.round(3/guesses * 100) + "%";
+alert(stats);
